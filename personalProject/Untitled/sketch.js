6028488afb7c5aaefc0a94b56604d7e43565bb2e
rajsaamjulia <juliaracsko@gmail.com>
2016-11-03 21:55:13 +0100

    create slider

--- personalProject/Untitled/sketch.js
+++ personalProject/Untitled/sketch.js
@@ -1,13 +1,19 @@
-var img;  // Declare variable 'img'.
+var img; // Declare variable 'img'.
+
+var timeSlider; //slider of the timeline
 
 function setup() {
   createCanvas(720, 400);
   img = loadImage("img.png");  // Load the image
+  timeSlider = createSlider(0, 30, 0);
+  timeSlider.position(20, 420);
+  timeSlider.size(700);
 }
 
 function draw() {
   // Displays the image at its actual size at point (0,0)
   image(img, 0, 0);
-  // Displays the image at point (0, height/2) at half size
-  image(img, 0, height/2, img.width/2, img.height/2);
+  var t = timeSlider.value(); // set the timeline that defines the changes
+  t = 0; 
+
 }
