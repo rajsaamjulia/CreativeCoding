ee3688d23a42707b8d1e0e05c3ee5a9ab3a6086e
rajsaamjulia <juliaracsko@gmail.com>
2016-11-07 14:13:25 +0100

    fix index-script connection

--- personalProject/script/sketchSlider.js
+++ personalProject/script/sketchSlider.js
@@ -1,19 +1,18 @@
 var img; // Declare variable 'img'.
-
 var timeSlider; //slider of the timeline
 
 function setup() {
-  createCanvas(720, 400);
+  createCanvas(800, 600);
   img = loadImage("img.png");  // Load the image
   timeSlider = createSlider(0, 30, 0);
-  timeSlider.position(20, 420);
+  timeSlider.position(20, 620);
   timeSlider.size(700);
 }
 
 function draw() {
   // Displays the image at its actual size at point (0,0)
-  image(img, 0, 0);
+  image(img, 20, 20);
   var t = timeSlider.value(); // set the timeline that defines the changes
-  t = 0; 
+  t = 0;
 
 }
